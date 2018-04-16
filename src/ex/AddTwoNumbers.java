ae14c621e0cca54ede0ed12621e640b98d785dac
谭皓文 <593659637@qq.com>
2018-04-16 18:15:10 +0800

    Update AddTwoNumbers.java
    Add Two Numbers

--- src/ex/AddTwoNumbers.java
+++ src/ex/AddTwoNumbers.java
@@ -3,7 +3,6 @@ package ex;
 public class AddTwoNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ListNode l1=new ListNode(2);
 		l1.next=new ListNode(4);
 		l1.next.next=new ListNode(3);
